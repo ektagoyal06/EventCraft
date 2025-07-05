d66defa91199c805ef152526312ea3609b8d2e44
Ekta Goyal <160715168+ektagoyal06@users.noreply.github.com>
2025-07-06 00:31:36 +0530

    Add files via upload

---
+++ backend/models/Booking.js
@@ -0,0 +1,19 @@
+// models/Booking.js
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+  eventName: String,
+  eventDate: String,
+  location: String,
+  image: String,
+  quantity: Number,
+  price: Number,
+  total: Number,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userName: String,
+  userEmail: String,
+  userContact: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model('Booking', bookingSchema);
