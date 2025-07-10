45394c023b5b8b2f6c14abd9f11657853942b160
Ekta <ektagoyal6904@gmail.com>
2025-07-11 00:24:50 +0530

    Removed .env from tracking and updated modified files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -39,45 +39,4 @@ const App = () => (
 );
 
 export default App;
-=======
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home'; // ✅ Make sure the file name is `Home.jsx`
-import RSVPPage from './pages/RSVPPage';
-import EventForm from './pages/EventForm';
-import EventsPage from './pages/EventsPage';
-import CalendarReminders from './pages/CalendarsReminders';
-import DashboardPage from './pages/DashboardPage';
-import Dashboard_org from './pages/Dashboard_org';
-import Payment from './pages/Payment';
-import SignupForm from './pages/SignupForm';
-// import Login from './pages/Login';
-import ComedyNights from './pages/ComedyNights';
-import User from './pages/User';
-import LandingPage from './pages/LandingPage';
-import ChatBot from './pages/ChatBot';
-// import Reminder from './pages/Reminder';
-
-const App = () => (
-  <Routes>
-    <Route path="/" element={<LandingPage />} />
-    {/* <Route path="/login" element={<Login/>} /> */}
-    <Route path="/dashboardPage" element={<DashboardPage />} />
-    <Route path="/dashboardorg" element={<Dashboard_org />} />
-    <Route path="/signup" element={<SignupForm/>} />
-    <Route path="/comedy" element={<ComedyNights/>} />
-    <Route path="/home" element={<Home />} />
-    <Route path="/create-event" element={<EventForm />} />
-    <Route path="/rsvp" element={<RSVPPage />} />
-    <Route path="/events" element={<EventsPage/>}/>
-    <Route path="/payment" element={<Payment/>} />
-    <Route path="/user" element={<User/>} />
-    <Route path="/chatbot" element={<ChatBot/>} />
-    {/* <Route path="/reminder" element={<Reminder/>} /> */}
-    <Route path="/calendar-reminders" element={<CalendarReminders />} />
-  </Routes>
-);
-
-export default App;
->>>>>>> 87a766492b4d90c9d5291afe21c40a8a81105199
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { BrowserRouter } from 'react-router-dom';
@@ -11,16 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </Router>
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
->>>>>>> 87a766492b4d90c9d5291afe21c40a8a81105199
